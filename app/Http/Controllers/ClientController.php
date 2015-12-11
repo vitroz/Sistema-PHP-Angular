808a2b38eaa260d32ff244efee937878f43402df
= <=>
2015-12-11 11:23:13 -0300

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \CodeProject\Client::all();
+    }
+
+   
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return \CodeProject\Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return \CodeProject\Client::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        \CodeProject\Client::find($id)->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        \CodeProject\Client::find($id)->delete();
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('client', 'ClientController@index');
+Route::get('client/{id}', 'ClientController@show');
+Route::post('client', 'ClientController@store');
+Route::delete('client/{id}', 'ClientController@destroy');
+Route::put('client/{id}', 'ClientController@update');
